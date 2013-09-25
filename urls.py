b7e29b6e770c00e021527ca5e24864d50e2ad552
webpioner <webpioner@gmail.com>
2013-09-25 15:54:56 -0700

    modified : site en construction

--- urls.py
+++ urls.py
@@ -19,8 +19,11 @@ urlpatterns = patterns("",
 
     # Cartridge URLs.
     ("^shop/", include("cartridge.shop.urls")),
+
     url("^account/orders/$", "cartridge.shop.views.order_history",
         name="shop_order_history"),
+    # main URLs
+    ("^", include("main.urls")),
 
      # We don't want to presume how your homepage works, so here are a
     # few patterns you can use to set it up.
